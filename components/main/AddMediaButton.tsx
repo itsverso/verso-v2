8f73bdf5cb65c81b27f866fd150aa38d9a06beeb
HugoSanchez <hugosanchez251@gmail.com>
2023-12-19 16:37:22 +0000

    fixed icons, prepared to mint

--- components/main/AddMediaButton.tsx
+++ components/main/AddMediaButton.tsx
@@ -1,20 +1,35 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { Plus, Pencil, Image } from "@/resources/icons";
+import React, { useState, useCallback } from "react";
+
+import { PencilIcon, PhotoIcon, PlusIcon } from "@heroicons/react/24/solid";
 
 export function AddMediaButton() {
-	return (
-		<div className="fixed bottom-10 right-10 flex flex-col items-center">
-			<div className="my-1 flex items-center justify-center  h-10 w-10 rounded-full bg-teal-400 hover:opacity-90 cursor-pointer shadow-2xl">
-				<Image size="6" color="black" />
-			</div>
+	const [renderButtons, setRenderButtons] = useState<boolean>(false);
 
-			<div className="my-1 flex items-center justify-center  h-10 w-10 rounded-full bg-teal-400 hover:opacity-90 cursor-pointer shadow-2xl">
-				<Pencil size="6" color="black" />
-			</div>
-			<div className="my-1 flex items-center justify-center  h-14 w-14 rounded-full bg-teal-400 hover:opacity-90 cursor-pointer shadow-2xl">
-				<Plus size="6" color="black" />
-			</div>
+	const handleRenderButtons = useCallback(() => {
+		setRenderButtons(!renderButtons);
+	}, [renderButtons]);
+
+	return (
+		<div className="fixed bottom-10 right-12 flex flex-col items-center">
+			{renderButtons ? (
+				<>
+					<div className="my-1 flex items-center justify-center  h-10 w-10 rounded-full bg-black hover:opacity-90 cursor-pointer shadow-2xl">
+						<PhotoIcon className="w-4 h-4 text-white" />
+					</div>
+
+					<div className="my-1 flex items-center justify-center  h-10 w-10 rounded-full bg-black hover:opacity-90 cursor-pointer shadow-2xl">
+						<PencilIcon className="w-4 h-4 text-white" />
+					</div>
+				</>
+			) : null}
+
+			<button
+				onClick={handleRenderButtons}
+				className="my-1 flex items-center justify-center  h-14 w-14 rounded-full bg-black hover:opacity-90 cursor-pointer shadow-2xl"
+			>
+				<PlusIcon className="w-6 h-6 text-white" />
+			</button>
 		</div>
 	);
 }
--- resources/icons.tsx
+++ resources/icons.tsx
@@ -121,16 +121,16 @@ export const Plus = ({ size = "6", color = "currentColor" }) => {
 	);
 };
 
-export const Image = ({ size = "2", color = "currentColor" }) => (
+export const Image = ({ size = "4", color = "currentColor" }) => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 52 52"
+		viewBox="0 0 24 24"
 		fill="none"
 		stroke={color}
-		strokeWidth="1.2"
+		strokeWidth="1.5"
 		strokeLinecap="round"
 		strokeLinejoin="round"
-		className={`w-2 h-2`}
+		className={`w-${size} h-${size}`}
 	>
 		<path
 			strokeLinecap="round"
@@ -140,20 +140,20 @@ export const Image = ({ size = "2", color = "currentColor" }) => (
 	</svg>
 );
 
-export const Pencil = ({ size = "6", color = "currentColor" }) => {
+export const Pencil = ({ size = "4", color = "currentColor" }) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
-			strokeWidth={1}
-			stroke={color}
-			className={`w-${size} h-${size}`}
+			strokeWidth={1.5}
+			stroke="currentColor"
+			className="w-4 h-4"
 		>
 			<path
 				strokeLinecap="round"
 				strokeLinejoin="round"
-				d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+				d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
 			/>
 		</svg>
 	);
