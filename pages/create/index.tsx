b2439a8491b6f62f3202220f0b02148c0954d8ed
HugoSanchez <hugosanchez251@gmail.com>
2024-01-09 16:35:46 +0000

    small changes

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,9 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { NextPage } from "next";
 import { Info } from "@/resources/icons";
+import { AppContext } from "@/context/context";
 import { CreateCollectionForm } from "@/components/forms/CreateCollectionForm";
 
 const Create: NextPage = () => {
+	let { state, dispatch } = useContext(AppContext);
+
+	if (!state.user.handle) {
+		return (
+			<main className="flex flex-row min-h-screen min-w-screen items-center justify-center">
+				<p>Only registered users can create</p>
+			</main>
+		);
+	}
+
 	return (
 		<main className="flex flex-row min-h-screen lg:px-96">
 			<CreateCollectionForm />
