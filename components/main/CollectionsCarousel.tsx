a49144ce7937bf6cebdc039825c5dcc15450e271
HugoSanchez <hugosanchez251@gmail.com>
2024-01-31 14:04:15 +0000

    mobile styles for profile

--- components/main/CollectionsCarousel.tsx
+++ components/main/CollectionsCarousel.tsx
@@ -4,47 +4,49 @@ import useGetUserCollections from "@/hooks/useGetUserCollections";
 import { EmptyUserGrid } from "./EmptyUserGrid";
 
 type Collection = {
-  address: string;
-  title: string;
-  description: string;
-  image: string;
+	address: string;
+	title: string;
+	description: string;
+	image: string;
 };
 
 type CarouselProps = {
-  handle: string;
-  isOwner: boolean;
-  openDrawer: () => void;
+	handle: string;
+	isOwner: boolean;
+	openDrawer: () => void;
 };
 
 export function CollectionsCarousel(props: CarouselProps) {
-  const { handle } = props;
-  const { data, error, isLoading } = useGetUserCollections(handle);
+	const { handle } = props;
+	const { data, error, isLoading } = useGetUserCollections(handle);
 
-  if (isLoading) {
-    return <div></div>;
-  }
+	if (isLoading) {
+		return <div></div>;
+	}
 
-  if (!error && data.collections) {
-    return (
-      <div className="w-full flex flex-col lg:flex-row items-center">
-        {data.collections.ownedNfts.length == 0 ? (
-          <div>
-            <EmptyUserGrid isOwner={props.isOwner} />
-          </div>
-        ) : (
-          <div className="w-full grid grid-cols-2 lg:grid md:grid-cols-3 gap-6 lg:gap-12 mt-6">
-            {data.collections.ownedNfts.map((item: any, index: any) => (
-              <CollectionCard
-                key={index}
-                item={item}
-                handle={props.handle}
-                title={item.title}
-                address={item.address}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
+	if (!error && data.collections) {
+		return (
+			<div className="w-full flex flex-col lg:flex-row items-center">
+				{data.collections.ownedNfts.length == 0 ? (
+					<div>
+						<EmptyUserGrid isOwner={props.isOwner} />
+					</div>
+				) : (
+					<div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-12 mt-6">
+						{data.collections.ownedNfts.map(
+							(item: any, index: any) => (
+								<CollectionCard
+									key={index}
+									item={item}
+									handle={props.handle}
+									title={item.title}
+									address={item.address}
+								/>
+							)
+						)}
+					</div>
+				)}
+			</div>
+		);
+	}
 }
