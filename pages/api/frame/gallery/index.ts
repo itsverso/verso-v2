ef5cbfcf725328ad7417bbcbc2e2aaad987bbfae
HugoSanchez <hugosanchez251@gmail.com>
2024-02-29 12:18:46 +0000

    lets see

--- pages/api/frame/gallery/index.ts
+++ pages/api/frame/gallery/index.ts
@@ -30,23 +30,39 @@ export default async function handler(
 	let body = req.body;
 	console.log(body);
 
-	let nextToken = tokenId + 1;
-	/** 
-	if (body.untrustedData.buttonIndex == 1) {
+	let nextToken = tokenId;
+	if (tokenId == 0) nextToken = 0;
+	else if (body.untrustedData.buttonIndex == 1) {
 		nextToken = tokenId - 1;
 	} else if (body.untrustedData.buttonIndex == 2) {
 		nextToken = tokenId + 1;
 	}
-	*/
 
-	if (nextToken < maxLength) {
+	if (nextToken == 0) {
+		// If token id is 0 and user has clicked on "Get started"
+		// We return second image and button options
+		let htmlResponse = `<!DOCTYPE html><html><head>
+				<title>This is frame 7</title>
+				<meta property="fc:frame" content="vNext" />
+				<meta property="fc:frame:image" content="${
+					data.tokens.nfts[1].rawMetadata.image
+				}" />
+				<meta property="fc:frame:button:1" content="Get started" />
+				<meta property="fc:frame:post_url" content="${
+					process.env.NEXT_PUBLIC_BASE_URL
+				}/frame/gallery?collectionAddress=${address}&tokenId=${1}" />
+			</head></html>`;
+		res.setHeader("Content-Type", "text/html");
+		res.status(200).send(htmlResponse);
+	} else if (nextToken < maxLength) {
 		// If token id is 0 and user has clicked on "Get started"
 		// We return second image and button options
 		let htmlResponse = `<!DOCTYPE html><html><head>
 				<title>This is frame 7</title>
 				<meta property="fc:frame" content="vNext" />
 				<meta property="fc:frame:image" content="${data.tokens.nfts[nextToken].rawMetadata.image}" />
-				<meta property="fc:frame:button:1" content="Next" />
+				<meta property="fc:frame:button:1" content="Previous" />
+				<meta property="fc:frame:button:2" content="Next" />
 				<meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/frame/gallery?collectionAddress=${address}&tokenId=${nextToken}" />
 			</head></html>`;
 		res.setHeader("Content-Type", "text/html");
